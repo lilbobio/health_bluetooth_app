de108e6d96a244e6b24496e3c29d3ea8d6eb0820
lilbobio <daoaldon@gmail.com>
2023-06-21 12:25:54 -0400

    setting up the app

--- bluetooth_healt_app/lib/main.dart
+++ bluetooth_healt_app/lib/main.dart
@@ -15,10 +15,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MIE Scale App',
       theme: ThemeData(
-        brightness: Brightness.light,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade700)
+          brightness: Brightness.light,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade700)),
+      home: const MyHomePage(
+        title: 'MIE Bluetooth Scale App',
       ),
-      home: const MyHomePage(title: 'MIE Bluetooth Scale App'),
     );
   }
 }
@@ -30,58 +31,103 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  void scan() {
-    bool isBluetooth = false;
-    QuickBlue.isBluetoothAvailable().then((result) => isBluetooth = result);
-    if(!isBluetooth){
-        print('device cannot connect');
-
-      return;
-    }
-
-    QuickBlue.scanResultStream.listen((event) {
-        print('onScanResult $event');
-    });
-
-
-    //QuickBlue.startScan();
-
-   // QuickBlue.
-  }
-
-  void connect() {
-    scan();
-  }
+  String connectedString = 'Click to Connect to Bluetooth Device\n\n\n';
+  _BlueTooth blueTooth = _BlueTooth();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Connect to Bluetooth device\n', 
+            Align(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                //width: 130,
+                child: SizedBox(
+                  child: Image.asset('assets/images/logo.jpg'),
+                ),
+              ),
             ),
-            FloatingActionButton(
-              onPressed: connect,
-              heroTag: null,
-              child: const Icon(Icons.bluetooth),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    connectedString,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () async {
+                        // ignore: await_only_futures, unrelated_type_equality_checks
+                        if(await blueTooth.connect == true){
+                          setState(() {
+                            connectedString = 'Bluetooth Device can be connected\n\n\n';
+                          });
+                        }else{
+                          setState(() {
+                            connectedString = 'Bluetooth is not enabled on device\n\n\n';
+                          });
+                        }
+                      },
+                    heroTag: null,
+                    child: const Icon(Icons.bluetooth),
+                  ),
+                ],
+              ),
             ),
-
           ],
         ),
-      ), 
+      ),
     );
+  }
+}
+
+class ConnectedPage extends StatefulWidget {
+  const ConnectedPage({super.key, required this.title});
+
+  final String title;
+  @override
+  State<ConnectedPage> createState() => _ConnectedPageState();
+}
+
+class _ConnectedPageState extends State<ConnectedPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class _BlueTooth {
+  Future<bool> checkIfBTIsEnabled() async {
+    if (await QuickBlue.isBluetoothAvailable()) {
+      print('device cannot connect');
+      return false;
+    } else {
+      print('device can connect');
+      return true;
+    }
+  }
+
+  Future<bool> connect() async {
+    if (!await checkIfBTIsEnabled()) {
+      return false;
+    }
+    // QuickBlue.scanResultStream.listen((event) {
+    //     print('onScanResult $event');
+    // });
+
+    //QuickBlue.startScan();
+
+    // QuickBlue.
+
+    return true;
   }
 }
