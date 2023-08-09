f875afdab41147ee54cf30f4509ac014208af44a
lilbobio <daoaldon@gmail.com>
2023-08-09 19:44:31 -0400

    added a TODO in the test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bluetooth_health_app/device_page.dart';
 
+//TODO: fix this test
 void main() {
   testWidgets('Tests if device page has a title', (tester) async {
     await tester.pumpWidget(const DevicePage(title: 'T'));
