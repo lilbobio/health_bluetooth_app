6d8a631b88d8e73025063266e6843c14c98a56d5
lilbobio <daoaldon@gmail.com>
2023-08-09 19:39:35 -0400

    added a test to see if there is only 1 title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bluetooth_health_app/main.dart';
 import 'package:bluetooth_health_app/device_page.dart';
 
 void main() {
-  testWidgets('Tests of device_page has all buttons', (tester) async {
+  testWidgets('Tests if device page has a title', (tester) async {
     await tester.pumpWidget(const DevicePage(title: 'T'));
+    final titleFinder = find.text('T');
+
+    expect(titleFinder, findsOneWidget);
   });
 }
